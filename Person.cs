d1429f805baa2731a23ddddd975deb33d179f12a
Naveen V <jayamnaveen68@gmail.com>
2022-02-14 17:00:31 +0530

    Use Case 1
    Ability to create the addressbook contains the details of different person

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingCSVandJSON
+{
+    internal class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public string City;
+        public string State;
+
+        public Person(string ssn, string name, string addr, string city, string state)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            City = city;
+            State = state;
+        }
+    }
+}
