b975b0260a097ac6534030b60121e41384a8782e
Naveen V <jayamnaveen68@gmail.com>
2022-02-14 17:14:15 +0530

    Use Case 2
    Adding New contacts of the person

---
+++ AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingCSVandJSON
+{
+    internal class AddressBook
+    {
+        // UC1
+        public static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", "Newyork","Newyork"));
+            listPersonInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork,NY", "Newyork", "Newyork"));
+            listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork,NY", "Newyork", "Newyork"));
+            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", "Newyork", "Newyork"));
+            listPersonInCity.Add(new Person("203456880", "SAM", "Senthil Nagar", "Uthangarai","Tamilnadu"));
+            listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", "Newyork", "Newyork"));
+            listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", "Newyork", "Newyork"));
+            listPersonInCity.Add(new Person("203456883", "Naveen", "5th Street,Senthilnagar", "Perungudi","Chennai"));
+            listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", " Baltimore", " Baltimore"));
+            Console.WriteLine(listPersonInCity.ToString());
+            listPersonInCity.ForEach(x => Console.WriteLine("{0}\t", x.Name.ToString()));
+
+        }
+        //UC2 to add person name using console
+        public static void AddPerson(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person());
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingCSVandJSON
+{
+    internal class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public string City;
+        public string State;
+
+        public Person()
+        {
+            Console.WriteLine("\nEnter Person details");
+            Console.WriteLine("\nSSN :");
+            SSN = Console.ReadLine();
+            Console.WriteLine("\nName :");
+            Name = Console.ReadLine();
+            Console.WriteLine("\nAddress :");
+            Address = Console.ReadLine();
+            Console.WriteLine("\nCity :");
+            City = Console.ReadLine();
+            Console.WriteLine("\nState :");
+            State = Console.ReadLine();
+        }
+
+        public Person(string ssn, string name, string addr, string city, string state)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            City = city;
+            State = state;
+        }
+    }
+}
