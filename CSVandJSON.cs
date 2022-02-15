ad02f061433ad03bd51874fe746b1fe1e47f3bea
Naveen V <jayamnaveen68@gmail.com>
2022-02-15 15:36:28 +0530

    Use Cases 14 and 15
    AddressBook using CSV file and JSON file

---
+++ CSVandJSON.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace AddressBookUsingCSVandJSON
+{
+    internal class CSVandJSON
+    {
+        public static void ImplemntCSVjSON()
+        {
+            string importFilePath = @"C:\Users\jayam\source\repos\AddressBookUsingCSVandJSON\AddressBookUsingCSVandJSON\Files\TextFile1.txt";
+            string exportFilePath = @"C:\Users\jayam\source\repos\AddressBookUsingCSVandJSON\AddressBookUsingCSVandJSON\Files\WriteAddressBookCSV.csv";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Person>().ToList();
+                Console.WriteLine("Read data successfully from Adress Book csv.");
+                foreach (Person person in records)
+                {
+                    Console.Write("\t" + person.SSN);
+                    Console.Write("\t" + person.Name);
+                    Console.Write("\t" + person.Address);
+                    Console.Write("\t" + person.City);
+                    Console.Write("\t" + person.State);
+                }
+                Console.WriteLine("**********************Reading fromcsv file and Write to csv file **************************");
+                //Writing json file
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportFilePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
--- Person.cs
+++ Person.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace AddressBookUsingCSVandJSON
 {
@@ -36,6 +37,43 @@ namespace AddressBookUsingCSVandJSON
             Address = addr;
             City = city;
             State = state;
+
+            string path = @"C:\Users\jayam\source\repos\AddressBookUsingCSVandJSON\AddressBookUsingCSVandJSON\Files\AddressBookTextFile.txt";
+            string lines;
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("{0},{1},{2},{3}", SSN, Name, Address, City, State);
+                sr.Close();
+                lines = File.ReadAllText(path);
+                Console.WriteLine(lines);
+
+            }
+            //string importFilePath = @"C:\Users\vgj24\source\repos\AddressBookProject10FebAssgn\Adressbokkcsv\AddressBookCSV.csv";
+            //string exportFilePath = @"C:\Users\vgj24\source\repos\AddressBookProject10FebAssgn\Adressbokkcsv\writeAddressBookCSV.csv";
+            //using (var reader = new StreamReader(importFilePath))
+            //using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            //{
+            //    var records = csv.GetRecords<Person>().ToList();
+            //    Console.WriteLine("Read data successfully from Adress Book csv.");
+            //    foreach (Person person in records)
+            //    {
+            //        Console.Write("\t" + SSN);
+            //        Console.Write("\t" + Name);
+            //        Console.Write("\t" + Address);
+            //        Console.Write("\t" + Age);
+
+            //        Console.WriteLine("**********************Reading fromcsv file and Write to csv file **************************");
+            //        //Writing json file
+            //        JsonSerializer serializer = new JsonSerializer();
+            //        using (StreamWriter sw = new StreamWriter(exportFilePath))
+            //        using (JsonWriter writer = new JsonTextWriter(sw))
+            //        {
+            //            serializer.Serialize(writer, records);
+            //        }
+            //    }
+
+            //}
+
         }
     }
 }
