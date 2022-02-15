8caa70d0362416824fa40934592eb81673e880eb
Naveen V <jayamnaveen68@gmail.com>
2022-02-15 15:23:24 +0530

    UseCase13
    Read and write The address Book details using file IO

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using AddressBookUsingCSVandJSON;
 
 namespace AddressBookUsingCSVandJSON
 {
@@ -7,7 +7,19 @@ namespace AddressBookUsingCSVandJSON
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the AddressBook");
+            Console.WriteLine("Welcome to Address Book Program");
+            List<Person> listPersonInCity = new List<Person>();
+            AddressBook AB  = new AddressBook();
+
+            AddressBook.AddRecords(listPersonInCity); //uc1
+            //AddressBook.AddPerson(listPersonInCity); //uc2
+            //AddressBook.edit(listPersonInCity);
+            //AddressBook.DeletepersonName(listPersonInCity);
+            //AddressBook.duplicateentry(listPersonInCity);
+            //AddressBook.SameCityStateMates(listPersonInCity);
+            //AddressBook.SortData(listPersonInCity);
+            //AddressBook.SortDatabyCity(listPersonInCity);
+            CSVJSON.ImplemntCSVjSON();
         } 
     }
 }
